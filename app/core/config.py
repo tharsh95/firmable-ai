bb2d65f40dddb8ec46947e937e99ed84af545cd6
Harsh Tiwari <harsh@kimo.ai>
2025-01-05 13:11:52 +0530

    Initial commit

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY","")
     MAX_CONTENT_LENGTH = 4000
     SECRET_KEY = "secret"
 
